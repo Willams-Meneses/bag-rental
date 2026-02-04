45920ab0ba71fd962bba32a57671f44c5d4d6791
Willams Meneses <menesestapia@gmail.com>
2026-02-03 22:13:12 -0300

    Added material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@mui/material"
 
 function App() {
 
   return (
     <>
      <div>hello word</div>
+     <Button variant="contained" color="primary">Test primary button</Button>
+     <Button variant="contained" color="secondary">Secondary button</Button>
     </>
   )
 }
