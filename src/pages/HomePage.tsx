603fb49e2adcc09c977ce675f19403ba841a07a1
Willams Meneses <menesestapia@gmail.com>
2026-02-03 22:24:35 -0300

    added basi index.css and test home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import { Checkbox, TextField } from "@mui/material"
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <div style={{
+        display: "flex",
+        alignItems: "flex-start"
+      }}>
+        <TextField placeholder="Test Input" />
+        <Checkbox />
+      </div>
+
+      Form login
+
+    </div>
+  )
+}
+
+export default HomePage
