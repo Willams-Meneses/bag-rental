bfcad633a06973caa1208aa2288ecbfe2ffe6622
Willams Meneses <menesestapia@gmail.com>
2026-02-04 22:43:37 -0300

    added more settings to typography and fix checkbox of material ui

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -54,18 +54,34 @@ export const typographyConfig: TypographyVariantsOptions = {
 
   subtitle1: {
     fontFamily: '"Plus Jakarta Sans", sans-serif',
+    fontSize: '18px',
+    fontWeight: 600,
+    lineHeight: '32px',
+    color: '#595959',
+
   },
 
   subtitle2: {
     fontFamily: '"Plus Jakarta Sans", sans-serif',
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: '24px',
+    color: '#737373',
   },
 
   body1: {
     fontFamily: '"Plus Jakarta Sans", sans-serif',
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: '24px'
   },
 
   body2: {
     fontFamily: '"Plus Jakarta Sans", sans-serif',
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: '20px',
+    color: '#737373'
   },
 
   button: {
@@ -78,6 +94,10 @@ export const typographyConfig: TypographyVariantsOptions = {
 
   caption: {
     fontFamily: '"Plus Jakarta Sans", sans-serif',
+    fontSize: '12px',
+    fontWeight: 400,
+    lineHeight: '20px',
+    color: '#4E4E4E'
   },
 
   overline: {
