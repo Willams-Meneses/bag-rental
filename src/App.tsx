e583f592078c3a6fbce2e6280e08a6ebe9b881c0
Willams Meneses <menesestapia@gmail.com>
2026-02-04 22:40:48 -0300

    Added global toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import theme from './theme/theme';
 import HomeNavbar from './components/ui/HomeNavbar';
+import GlobalToast from './components/ui/GlobalToast';
 
 // pages
 import HomePage from './pages/HomePage';
@@ -13,8 +14,8 @@ function App() {
         <HomeNavbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-
         </Routes>
+        <GlobalToast />
       </BrowserRouter>
     </ThemeProvider>
   );
